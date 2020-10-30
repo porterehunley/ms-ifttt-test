bc136ef83c71ad06091e91332ec87d51dc545f71
Porter Hunley <t-phunley@microsoft.com>
2020-10-30 18:44:02 -0400

    status endpoint

--- testapi.py
+++ testapi.py
@@ -1,8 +1,15 @@
 from flask import Flask 
+from flask import request
 
 app = Flask(__name__)
+service_key = "3aKjTXxu2zoRqxr1rZ5nkniI56JWOTo51gwgCu7hZfbxmkiVIJEt4e5q_E3lxEOL"
 
 @app.route('/')
 def index():
     return "hello world"
 
+@app.route('/ifttt/v1/status', methods=['GET'])
+def status():
+    if ("IFTTT-Service-Key" not in request.headers or request.headers["IFTTT-Service-Key"] != service_key):
+        return "", 503        
+    return "", 200

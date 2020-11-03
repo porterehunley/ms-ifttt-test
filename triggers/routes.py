9aade126dfb2f7f0f26387c0eda19f018b55dfca
Porter Hunley <t-phunley@microsoft.com>
2020-11-03 13:35:44 -0500

    adds service key

--- triggers/routes.py
+++ triggers/routes.py
@@ -12,9 +12,14 @@ trigger body:
 + user - info on user
 + ifttt_source - info on applet
 '''
+service_key = "3aKjTXxu2zoRqxr1rZ5nkniI56JWOTo51gwgCu7hZfbxmkiVIJEt4e5q_E3lxEOL"
 @bp.route('/new_date_created', methods=['POST', 'GET'])
 def new_date_created():
     data = request.get_json() or {}
+    if ("IFTTT-Service-Key" not in request.headers or request.headers["IFTTT-Service-Key"] != service_key):
+        return "", 401
+    
     limit = data['limit'] if 'limit' in data else 50
     ingrediants = [("date", (datetime.now()-timedelta(minutes=x)).isoformat()) for x in range(limit)]
     return jsonify(create_triggers_response(ingrediants))
+
